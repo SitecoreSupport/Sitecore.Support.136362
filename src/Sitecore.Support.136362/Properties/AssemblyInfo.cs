206541af1fe2dff30e0265e59de3196e4b97cc72
Dmitry Baranov <dbr@sitecore.net>
2016-12-01 16:28:32 +0200

    no message

---
+++ src/Sitecore.Support.136362/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.136362")]
+[assembly: AssemblyProduct("Sitecore.Support.136362")]
+[assembly: ComVisible(false)]
